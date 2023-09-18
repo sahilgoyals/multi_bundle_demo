f11d88a63f9b7302b735f6a7d5041aa5a6801bab
Sahil Goyal <sahil.goyal@housing.com>
2023-09-18 10:27:59 +0530

    change dev=false

--- src/views/appB/index.js
+++ src/views/appB/index.js
@@ -41,7 +41,7 @@ function App() {
 
   useEffect(() => {
     Sentry.init({
-      dsn: 'DSN_HERE',
+      dsn: 'https://1f1780d174c2e9412f15da802bb637c1@o4505861388173312.ingest.sentry.io/4505861389352960',
       dist: '2',
       // Set tracesSampleRate to 1.0 to capture 100%
       // of transactions for performance monitoring.
